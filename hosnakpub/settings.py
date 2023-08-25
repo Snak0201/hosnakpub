d6652c634a29b3319817d037207282e3b8ac9335
Snak <72hoshino.snak@gmail.com>
2023-08-25 13:38:30 +0900

    fix markdownx settings

--- hosnakpub/settings.py
+++ hosnakpub/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from uuid import uuid4
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,8 +152,16 @@ CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+# django-markdownx
+MARKDOWNX_MEDIA_PATH = f"{uuid4()}/"
+
 MARKDOWNX_MARKDOWN_EXTENSIONS = [
     "extra",
     "nl2br",
     "sane_lists"
 ]
+
+MARKDOWNX_IMAGE_MAX_SIZE = {
+    "size": (1920, 1080),
+    "quality": 80,
+}
