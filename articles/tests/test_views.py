db9d7c99e1fff99cb645bcb5fa8ed7c4a640c69f
Snak <72hoshino.snak@gmail.com>
2023-08-25 11:49:08 +0900

    fix test

--- articles/tests/test_views.py
+++ articles/tests/test_views.py
@@ -10,19 +10,23 @@ class ArticleListViewTest(TestCase):
     
     def test_get_view(self):
         self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, "articles/list.html")
+        self.assertTemplateUsed(self.response, "articles/list.html")      
     
-    def test_all_not_draft_article_in_order_of_new_updated(self):
-        self.assertEqual(self.response.context["articles"].count(), Article.objects.filter(is_draft=False).count())
+    def test_has_all_published_articles(self):
+        self.assertEqual(self.response.context["articles"].count(), Article.objects.filter(is_draft=False).count())  
+
+    def test_articles_are_in_order_of_new_updated(self):
+        pass
+
 
 class ArticleDetailViewTest(TestCase):
     fixtures = ["articles.json"]
 
     def setUp(self):
         self.response_not_draft = self.client.get(reverse("articles:detail", kwargs={"pk": 1}))
+        self.article_not_draft = Article.objects.filter(is_draft=False)
     
     def test_get_view_not_draft(self):
         self.assertEqual(self.response_not_draft.status_code, 200)
         self.assertTemplateUsed(self.response_not_draft, "articles/detail.html")
-        self.assertEqual(self.response_not_draft.context["article"], Article.objects.get(id=1))
-    
+        self.assertEqual(self.response_not_draft.context["article"], self.article_not_draft)
