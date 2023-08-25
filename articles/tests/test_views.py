24c29dd4442cb47685d9c9bc671a9079bc3294b6
Snak <72hoshino.snak@gmail.com>
2023-08-25 23:10:06 +0900

    add test

--- articles/tests/test_views.py
+++ articles/tests/test_views.py
@@ -24,6 +24,8 @@ class IndexViewTest(TestCase):
 
     def test_has_new_articles_space(self):
         self.assertContains(self.response, "最新記事", 1)
+        
+    def test_has_five_published_articles_in_order_of_new_updated(self):
         self.assertEqual(self.response.context["new_articles"].count(), 5)
         self.assertQuerysetEqual(self.response.context["new_articles"], Article.objects.filter(is_published=True).order_by("-updated_at")[:5])
 
@@ -62,3 +64,32 @@ class ArticleListViewTest(TestCase):
     def test_has_all_published_articles_in_order_of_new_updated(self):
         self.assertEqual(self.response.context["articles"].count(), Article.objects.filter(is_published=True).count())
         self.assertQuerysetEqual(self.response.context["articles"], Article.objects.filter(is_published=True).order_by("-updated_at"))
+
+
+class ArticleDetailViewTest(TestCase):
+    fixtures = ["articles.json"]
+
+    def setUp(self):
+        self.published_article = Article.objects.filter(is_published=True).first()
+        self.draft_article = Article.objects.filter(is_published=False).first()
+        self.response_published = self.client.get(reverse("articles:detail", kwargs={"pk": self.published_article.id}))
+        self.response_draft = self.client.get(reverse("articles:detail", kwargs={"pk": self.draft_article.id}))
+    
+    def test_get_view_published(self):
+        self.assertEqual(self.response_published.status_code, 200)
+        self.assertTemplateUsed("articles/detail.html")
+    
+    def test_has_right_title_published(self):
+        self.assertContains(self.response_published, f"<title>{self.published_article.title} | ほしのなか政府</title>")
+    
+    def test_has_article_element(self):
+        self.assertContains(self.response_published, f'<div id="title">{self.published_article.title}</div>')
+        self.assertContains(self.response_published, f'<div id="content">{self.published_article.content}</div>')
+        self.assertContains(self.response_published, f'<div id="created_at">{self.published_article.created_at}</div>')
+        self.assertContains(self.response_published, f'<div id="updated_at">{self.published_article.updated_at}</div>')
+    
+    def test_get_view_draft_without_login(self):
+        self.assertEqual(self.response_draft.status_code, 404)
+    
+    def test_has_right_title_draft(self):
+        self.assertContains(self.response_draft, "<title>ほしのなか政府</title>")
