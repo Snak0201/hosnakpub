bc65f64442fb7a75c3c4bde171e17f9dc5e64fcc
Snak <72hoshino.snak@gmail.com>
2023-08-25 10:03:12 +0900

    fix black format

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path("", views.IndexView.as_view(), name="index"),
 ]
