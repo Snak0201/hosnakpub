cd0d7ec13557cb5422601d644b2de0fe65cee0c6
Snak <72hoshino.snak@gmail.com>
2023-08-25 12:54:09 +0900

    fix column name

--- articles/models.py
+++ articles/models.py
@@ -6,6 +6,6 @@ from markdownx.models import MarkdownxField
 class Article(models.Model):
     title = models.CharField(verbose_name="タイトル", max_length=255)
     content = MarkdownxField(verbose_name="内容")
-    is_draft = models.BooleanField(verbose_name="下書き", default=True)
+    is_published = models.BooleanField(verbose_name="公開", default=False)
     created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="最終更新日時", auto_now=True)
--- articles/tests/test_views.py
+++ articles/tests/test_views.py
@@ -13,20 +13,7 @@ class ArticleListViewTest(TestCase):
         self.assertTemplateUsed(self.response, "articles/list.html")      
     
     def test_has_all_published_articles(self):
-        self.assertEqual(self.response.context["articles"].count(), Article.objects.filter(is_draft=False).count())  
+        self.assertEqual(self.response.context["articles"].count(), Article.objects.filter(is_published=True).count())  
 
     def test_articles_are_in_order_of_new_updated(self):
         pass
-
-
-class ArticleDetailViewTest(TestCase):
-    fixtures = ["articles.json"]
-
-    def setUp(self):
-        self.response_not_draft = self.client.get(reverse("articles:detail", kwargs={"pk": 1}))
-        self.article_not_draft = Article.objects.filter(is_draft=False)
-    
-    def test_get_view_not_draft(self):
-        self.assertEqual(self.response_not_draft.status_code, 200)
-        self.assertTemplateUsed(self.response_not_draft, "articles/detail.html")
-        self.assertEqual(self.response_not_draft.context["article"], self.article_not_draft)
