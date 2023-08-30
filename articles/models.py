b625e32eec1c4ac098098b15c50de2b30447dbe0
Snak <72hoshino.snak@gmail.com>
2023-08-30 14:22:20 +0900

    fix model

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,7 @@ from markdownx.models import MarkdownxField
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(verbose_name="タイトル", max_length=255)
-    content = MarkdownxField(verbose_name="内容")
+    content_with_markdown = MarkdownxField(verbose_name="内容のマークダウン記述")
     is_published = models.BooleanField(verbose_name="公開", default=False)
     created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="最終更新日時", auto_now=True)
