6e23d085923ee30b78911bf3c452472e27044412
Snak <72hoshino.snak@gmail.com>
2023-09-16 23:15:37 +0900

    add admin site

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
 
-from .models import Article
+from .models import Article, Bureau
 
 # Register your models here.
 admin.site.register(Article, MarkdownxModelAdmin)
+admin.site.register(Bureau)
